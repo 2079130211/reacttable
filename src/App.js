12203c61fe68fa5bff4e5d01de26c2ee1d2c9ffc
pawan sahu <pwn2957@gmail.com>
2023-03-25 20:20:58 +0530

    react table example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React , {useState,useMemo}from 'react';
+
+import DataTable from 'react-data-table-component';
+
+
+
+const columns = [
+  {
+      name: 'Title',
+      selector: row => row.title,
+  },
+  {
+      name: 'Year',
+      selector: row => row.year,
+  },
+];
+
+const data = [
+  {
+      id: 1,
+      title: 'Beetlejuice',
+      year: '1988',
+  },
+  {
+      id: 2,
+      title: 'Ghostbusters',
+      year: '1984',
+  },
+]
+
+
+
+
+export default () => {
+	const [filterText, setfilterText] = useState('');
+	
+  const [resetPaginationToggle, setResetPaginationToggle] = useState(false);
+	
+ 
+
+  const [filteredItems , setfilteredItems] = useState(data)
+
+  
+ 
+   
+
+
+ 
+    const test2 = (e)=>{
+      let restdata = [];
+      setfilterText(e.target.value);
+      console.log(filterText)
+        restdata = data.filter(item => {
+          if(item.title && item.title.toLowerCase().includes(e.target.value.toLowerCase())){
+            return item
+          }
+
+          if(item.year && item.year.toLowerCase().includes(e.target.value.toLowerCase())){
+            return item
+          }
+        }) 
+        return setfilteredItems(restdata)
+    }
+
+	  const handleClear = () => {
+      setfilteredItems(data);
+      setResetPaginationToggle(!resetPaginationToggle);
+      return setfilterText('');
+    };
+  
+ 
+ 
+ 
+
+
+	return (
+    <>
+    <input  type="search" onInput={e=>test2(e)}  name="search"    value={filterText}    />
+    <button  onClick={handleClear} >Clear</button>
+
+
+		<DataTable title="Contact List" columns={columns} data={filteredItems} 
+			paginationResetDefaultPage={resetPaginationToggle}  
+      pagination
+			subHeader
+      persistTableHead
+      // subHeaderComponent={subHeader  ComponentMemo}
+			// selectableRows 
+    
+		/>
+
+</>
+
+	);
 }
-
-export default App;
